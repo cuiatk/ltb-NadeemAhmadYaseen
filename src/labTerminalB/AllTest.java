194e155f21704fc34fd8c860207197324278acee
NadeemAhmadYaseen <48080098+NadeemAhmadYaseen@users.noreply.github.com>
2019-06-13 11:22:11 +0500

    Create AllTest.java

---
+++ src/labTerminalB/AllTest.java
@@ -0,0 +1,11 @@
+package labTerminalB;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CustomerTest.class, NewTest.class, Twotest.class })
+public class AllTests {
+
+}
